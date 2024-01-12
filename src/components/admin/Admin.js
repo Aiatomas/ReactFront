b8594f3a3c21668c17db0bbe4163729bd7655a90
Ergaf <Horus333777123>
2024-01-12 11:21:08 +0200

    Re

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -9,6 +9,10 @@ import {TableUsers} from "./table/TableUsers";
 import {TableFiles} from "./table/TableFiles";
 import {TableOrders} from "./table/TableOrders";
 import {TableActions} from "./table/TableActions";
+import {Button, Collapse} from "react-bootstrap";
+import {CollapseAi} from "./CollapseAi";
+import Desktop from "./crm/Desktop/Desktop";
+import Sklad from "./crm/Sklad";
 
 export const Admin = () => {
     const [whoPick, setWhoPick] = useState("devices");
@@ -16,6 +20,23 @@ export const Admin = () => {
     // buttons.push("devices")
     // buttons.push("materials")
     // buttons.push("services")
+
+
+    buttons.push("Робочий стіл")
+    buttons.push("Чати")
+    buttons.push("Доставка")
+    buttons.push("Завдання")
+    buttons.push("Угоди")
+    buttons.push("Каса")
+    buttons.push("Записи")
+    buttons.push("Прайс-лист")
+    buttons.push("Склад")
+    buttons.push("Виробництво")
+    buttons.push("Фінанси")
+    buttons.push("Документи")
+    buttons.push("Статистика")
+    buttons.push("Налаштування")
+
     buttons.push("sessions")
     buttons.push("users")
     // buttons.push("files")
@@ -26,13 +47,36 @@ export const Admin = () => {
     };
 
     return (
-        <div>
-            <div className="d-flex justify-content-center align-content-center align-items-center">
-                {buttons.map((item) => (
-                    <button onClick={pickFunc} className={item === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={item} key={item}>{item}</button>
-                ))}
+        <div className="d-flex">
+            <div className="d-flex flex-column">
+                {/*{buttons.map((item) => (*/}
+                {/*    <button onClick={pickFunc} className={item === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={item} key={item}>{item}</button>*/}
+                {/*))}*/}
+                <button onClick={pickFunc} className={"Робочий стіл" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Робочий стіл"}>{"Робочий стіл"}</button>
+                <button onClick={pickFunc} className={"Чати" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Чати"}>{"Чати"}</button>
+                <button onClick={pickFunc} className={"Доставка" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Доставка"}>{"Доставка"}</button>
+                <button onClick={pickFunc} className={"Завдання" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Завдання"}>{"Завдання"}</button>
+                <button onClick={pickFunc} className={"Угоди" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Угоди"}>{"Угоди"}</button>
+                <button onClick={pickFunc} className={"Каса" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Каса"}>{"Каса"}</button>
+                <button onClick={pickFunc} className={"Записи" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Записи"}>{"Записи"}</button>
+                <button onClick={pickFunc} className={"Прайс-лист" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Прайс-лист"}>{"Прайс-лист"}</button>
+                <button onClick={pickFunc} className={"Склад" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Склад"}>{"Склад"}</button>
+                <button onClick={pickFunc} className={"Виробництво" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Виробництво"}>{"Виробництво"}</button>
+                <button onClick={pickFunc} className={"Фінанси" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Фінанси"}>{"Фінанси"}</button>
+                <button onClick={pickFunc} className={"Документи" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Документи"}>{"Документи"}</button>
+                <button onClick={pickFunc} className={"Статистика" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Статистика"}>{"Статистика"}</button>
+                <button onClick={pickFunc} className={"Налаштування" === whoPick ? 'btn btnm fileActive' : 'btn btnm'} toclick={"Налаштування"}>{"Налаштування"}</button>
+                <CollapseAi/>
             </div>
             {/*<Table name={whoPick}/>*/}
+            {whoPick === "Робочий стіл" &&
+                <Desktop/>
+            }
+            {whoPick === "Склад" &&
+                <Sklad/>
+            }
+
+
             {whoPick === "devices" &&
                 <TableDevices name={whoPick}/>
             }
@@ -57,6 +101,7 @@ export const Admin = () => {
             {whoPick === "actions" &&
                 <TableActions name={whoPick}/>
             }
+
         </div>
     )
 }
