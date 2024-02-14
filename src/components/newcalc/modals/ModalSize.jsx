22de60a1c9516721db0112158fdf36af198e28c6
Aiatomas <hello@printpeaks.com.ua>
2024-02-14 13:11:44 +0200

    f

--- src/components/newcalc/modals/ModalSize.jsx
+++ src/components/newcalc/modals/ModalSize.jsx
@@ -24,7 +24,7 @@ const ModalSize = ({size, setSize}) => {
         setShow(true);
     }
 
-    let deleteThisRowF = (e) => {
+    let setCustomValues = (e) => {
         let validX = true;
         let validY = true;
         setXVal(false)
@@ -46,6 +46,43 @@ const ModalSize = ({size, setSize}) => {
         }
     }
 
+    const handleSelectOption = e => {
+        const selectedFormat = formats.find(format => format.name === e.target.value);
+        if(selectedFormat){
+            setX(selectedFormat.x);
+            setY(selectedFormat.y);
+        } else {
+
+        }
+    }
+
+    let formats = [
+        {name:"300х500 мм", x: 300, y: 500},
+        {name:"310x310 мм", x: 310, y: 310},
+        {name:"300х630 мм", x: 300, y: 630},
+        {name:"314х324 мм", x: 314, y: 324},
+        {name:"А6 (105 х 148 мм)", x: 105, y: 148},
+        {name:"A5 (148 х 210 мм)", x: 148, y: 210},
+        {name:"A4 (210 x 297 мм)", x: 210, y: 297},
+        {name:"А3 (297 х 420 мм)", x: 297, y: 420},
+        {name:"300х430 мм", x: 300, y: 430},
+        {name:"314х648 мм", x: 314, y: 648},
+        {name:"90х50 мм", x: 90, y: 50},
+        {name:"85x55 мм", x: 85, y: 55},
+        {name:"90х55 мм", x: 90, y: 55},
+        {name:"100х100 мм", x: 100, y: 100},
+        {name:"100х150 мм", x: 100, y: 150},
+        {name:"86х54 мм", x: 86, y: 54},
+        {name:"200х100 мм", x: 200, y: 100},
+        {name:"87х54 мм", x: 87, y: 54},
+        {name:"200х200 мм", x: 200, y: 200},
+        {name:"88х50 мм", x: 88, y: 50},
+        {name:"85х54 мм", x: 85, y: 54},
+        {name:"300х300 мм", x: 300, y: 300},
+        {name:"314х474 мм", x: 314, y: 474},
+        {name:"314х685 мм", x: 314, y: 685},
+    ]
+
     return (
         <>
             <Card onClick={handleShow}>
@@ -62,6 +99,20 @@ const ModalSize = ({size, setSize}) => {
                     <Modal.Title>Розмір</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <Form.Select aria-label="Default select example"
+                    onChange={handleSelectOption}
+                    >
+                        <option>Оберіть значення</option>
+                        <option>Задати свій розмір</option>
+                        {formats.map((item, iter) => (
+                            <option
+                                key={item.name}
+                            >
+                                {item.name}
+                            </option>
+                        ))}
+                    </Form.Select>
+
                     <InputGroup className="mb-3">
                         <InputGroup.Text id="basic-addon1">x</InputGroup.Text>
                         <Form.Control
@@ -99,7 +150,7 @@ const ModalSize = ({size, setSize}) => {
                     <Button variant="secondary" onClick={handleClose}>
                         Закрити
                     </Button>
-                    <Button variant="success" onClick={deleteThisRowF}>
+                    <Button variant="success" onClick={setCustomValues}>
                         Застосувати
                     </Button>
                 </Modal.Footer>
